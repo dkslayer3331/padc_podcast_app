391d1d15139123c628f6c0723de32e5db4ef8e9b
moehtet <laxus3331@gmail.com>
2020-09-01 00:24:12 +0630

    wip

---
+++ app/src/main/java/com/mhst/padc_podcast_app/activities/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.mhst.padc_podcast_app.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mhst.padc_podcast_app.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
